5cef8a4cc1b9684d9573e88cefb3c2812e17a665
Bernhard Alber <bernhard.alber@atos.net>
2022-12-23 10:50:10 +0100

    Create responsibleuse.blade.php

---
+++ responsibleuse.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="TripScout stellt deine wichtigste Ressource für deinen Drogenkonsum dar. Hier findest du Konsuminformationen zur Harm-Reduction." />
+        <meta name="author" content="" />
+        <title>TripScout | Contact | Die wichtigsten Infos über deine Drogen!</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{ asset('/css/styles.css') }}" rel="stylesheet" />
+        <link href="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('backend/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />  
+        <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
+    </head>
+    <body>
+    @yield('frontend')
+                @include('frontend.gtag')
+        <!-- Navigation-->
+        <nav class="navbar navbar-light bg-light static-top">
+            <div class="container">
+                <a class="navbar-brand" href="/">TripScout</a>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                <h3 class="card-title text-center">Verantwortungsvoller Umgang</h3>
+                </div>
+            </div>
+        </header>
+        <!-- Icons Grid-->
+        <section class="features-icons bg-light">
+            <div class="col-sm-8 mx-auto">
+                <div class="row">
+
+<div class="card mx-auto" style="width: 90%">
+  <div class="card-body">
+    <h5 class="card-title">Vorwort</h5>
+    <p class="card-text">Verantwortungsvoller Drogenkonsum ist ein Bündel verschiedener Ideen und Praktiken, die auf der Prämisse beruhen, dass Freizeitsubstanzen von einwilligenden erwachsenen Personen auf gesunde und verantwortungsvolle Weise konsumiert werden können. Er steht in engem Zusammenhang mit dem Konzept der Schadensminimierung, das darauf abzielt, die Risiken und Gefahren psychoaktiver Substanzen zu minimieren und gleichzeitig deren Nutzen durch Aufklärung und Schulung zu maximieren.<br />
+Nach dem Konzept der Schadensminimierung ist der Freizeitkonsum von Substanzen in erster Linie als eine risikoreiche Aktivität zu verstehen, die das Risiko von Abhängigkeit, schweren Körperverletzungen und Tod in sich birgt. Daher besteht die pragmatischste Strategie, die ein Einzelner verfolgen kann - abgesehen von der völligen Abstinenz, die nicht immer realistisch oder wünschenswert ist - darin, jede Substanz sorgfältig zu erforschen und praktische Schritte zu unternehmen, um die mit dem Konsum verbundenen Risiken und Schäden zu verringern, bis der Nutzen die Kosten überwiegt.<br />
+Der Freizeitdrogenkonsum kann in einem ähnlichen Licht gesehen werden wie andere riskante, aber bereichernde Aktivitäten, insbesondere Extremsportarten wie Segeln, Skifahren, Fallschirmspringen, Surfen und Bergsteigen. Ganz banal kann man ihn auch mit dem Auto- oder Motorradfahren oder dem Fliegen in einem Flugzeug vergleichen.<br />
+Diese Tätigkeiten sind zwar mit erheblichen Risiken (einschließlich des Todes) verbunden, doch ist man sich weitgehend darüber im Klaren, dass diese Risiken durch eine angemessene Ausbildung und Schulung auf ein akzeptables Maß reduziert werden können. Je nach kulturellem Kontext kann die Fähigkeit, an diesen Aktivitäten teilzunehmen, als unveräußerlicher Ausdruck der eigenen Freiheit, Selbstbestimmung und Würde angesehen werden.<br />
+Die Philosophie, die dem verantwortungsvollen Drogenkonsum zugrunde liegt, kann als relativ radikal bezeichnet werden, da sie dem Einzelnen die absolute Verantwortung auferlegt, ordnungsgemäße Forschung zu betreiben und die erforderlichen Sicherheitsvorkehrungen zu treffen. Damit einher geht die Einsicht, dass es so etwas wie einen wirklich "sicheren" Konsum nicht gibt, sondern nur einen "sicheren" Konsum, und dass der Einzelne letztlich für die Folgen seiner Entscheidungen verantwortlich ist, sei es in gesundheitlicher, finanzieller oder anderer Hinsicht.<br />
+Die Befürworter eines verantwortungsvollen Drogenkonsums verweisen auf die vielen bekannten Künstler und Intellektuellen, die - experimentell oder anderweitig - Drogen konsumiert haben, ohne dass sich dies nachteilig auf ihr Leben ausgewirkt hat. Kritiker argumentieren, dass Drogen eskapistisch, gefährlich, unberechenbar und oft süchtig machen; daher sei ein verantwortungsvoller Drogenkonsum eine Illusion.<br />
+Examples of general harm reduction advice include:
+<ul>
+    <li>sich über die Wirkung und Legalität der konsumierten Substanz zu informieren</li>
+    <li>genaue Dosierung und andere Vorsichtsmaßnahmen, um das Risiko einer Überdosierung zu verringern</li>
+    <li>sich die Zeit zu nehmen, alle konsumierten Substanzen chemisch zu testen, um ihre Reinheit und Stärke zu bestimmen</li>
+    <li>sich nicht ans Steuer zu setzen, schwere Maschinen zu bedienen oder anderweitig direkt oder indirekt für die Sicherheit oder Pflege einer anderen Person verantwortlich zu sein, während man berauscht ist</li>
+    <li>Einen Tripsitter zu haben, wenn man eine Substanz nimmt, mit der man nicht vertraut ist</li>
+    <li>Nicht versuchen, jemanden auszutricksen oder zu überreden, eine Substanz zu konsumieren, zu der er nicht bereit ist</li>
+    <li>Nicht zulassen, dass der Drogenkonsum andere Aspekte des eigenen Lebens oder der eigenen Verantwortung überschattet</li>
+    <li>Moralisches Bewusstsein über die Quelle der eigenen Substanzen</li>
+</ul>
+
+
+</p>
+  </div>
+</div>
+<div class="card mx-auto" style="width: 90%">
+  <div class="card-body">
+    <h5 class="card-title">Allgemein</h5>
+    <p class="card-text">
+<br /><h2>Dosierung</h2>
+Im Zusammenhang mit dem Freizeitkonsum von Substanzen ist die richtige Dosierung von entscheidender Bedeutung.<br />
+Die Wahl einer unangemessen hohen Dosis einer Substanz ist wegen des Risikos gefährlicher Nebenwirkungen natürlich problematisch. Wenn man jedoch mit einer zu niedrigen Dosis beginnt, kann sich der Konsument unwohl fühlen und frustriert sein, was zu einer unvorsichtigen Nachdosierung führen und das Risiko einer kumulativen Überdosierung erhöhen kann.<br />
+Ein hilfreiches Sprichwort aus der Praxis: "Man kann immer mehr nehmen, aber nie weniger"!<br />
+<br /><h2>Wahl der Dosierung</h2>
+Bei der Wahl der Dosis sollte der Benutzer Dosen vermeiden, mit denen er sich nicht wohl fühlt oder mit denen er nicht vertraut ist. Ein unerfahrener Anwender sollte immer am unteren Ende des Dosisbereichs beginnen und sich in kleinen Schritten hocharbeiten, bis er mit dem Verhalten der Substanz vertraut ist.<br />
+Die Dosis sollte immer in kleinen Schritten nach oben angepasst werden (z. B. 1/4 bis 1/2 der vorherigen Dosis). Große, sprunghafte Erhöhungen (z. B. auf das 2- oder 3-fache der vorherigen Dosis) sollten unbedingt vermieden werden. Es ist wichtig zu beachten, dass viele Substanzen keine lineare Dosis-Wirkungs-Kurve haben, was bedeutet, dass eine Verdoppelung der Dosismenge einen Anstieg um mehr als das Doppelte verursacht (und schnell zu überwältigenden, unangenehmen und potenziell gefährlichen Erfahrungen führt). Wenn Sie diese Regel befolgen, können Sie das Risiko negativer Erfahrungen erheblich minimieren.<br />
+Es ist wichtig, daran zu denken, dass jeder Mensch auf jede Substanz anders reagiert, abhängig von Faktoren wie der persönlichen Toleranz, dem Körpergewicht, dem Stoffwechsel und der persönlichen Empfindlichkeit. Ein weiterer zu berücksichtigender Faktor ist die Reinheit der Substanz, die von Anbieter zu Anbieter und von Charge zu Charge unterschiedlich sein kann.<br />
+<br /><h2>Allergietests</h2>
+Einige Personen, insbesondere solche mit gesundheitlichen Problemen, können unerwünschte Reaktionen auf Substanzen in Form von allergischen Reaktionen, unangenehmen körperlichen oder kognitiven Effekten oder Überempfindlichkeit zeigen[1]. Dieses Risiko kann durch einen "Allergietest" gemindert werden, bei dem einfach eine winzige Menge der Substanz (z. B. 1/10 bis 1/4 einer regulären Dosis) verabreicht wird und mehrere Stunden gewartet wird, um zu überprüfen, ob man keine ungewöhnliche oder eigenwillige Reaktion zeigt.<br />
+<br /><h2>Augenmaß</h2>
+Nicht mit dem Auge messen. Augenmaß ist eine äußerst ungenaue und unzuverlässige Methode zum Abmessen von Substanzen, bei der man eine Substanz betrachtet und die Menge grob schätzt. Da zwischen 10 Milligramm und 30 Milligramm kein großer Unterschied besteht, kann dies leicht dazu führen, dass zu viel von einer Substanz eingenommen wird, was fatale Folgen haben kann.<br />
+Es gibt zahlreiche Arten von Augenmaß-Methoden, darunter die Verwendung von Mikrolöffeln und die Millimeterpapier-Methode, die beide zu einer ungenauen Dosierung führen.<br />
+Die Konsumenten sollten sich nicht auf das Wort eines Verkäufers verlassen, dass sein Produkt eine bestimmte Menge wiegt, da es nicht selten vorkommt, dass der Verkäufer versehentlich oder absichtlich eine falsche Menge eines Produkts abgibt, was bei einigen Konsumenten zu einer Überdosierung führt.</br>
+Besonders gefährlich ist es, Substanzen mit extrem niedrigen Dosen (unter 10 mg) zu beäugen.<br />
+<br /><h2>Milligramm Waagen</h2>
+Beim Kauf oder der Verwendung von Chemikalien in Pulverform wird dringend empfohlen, in eine zuverlässige und genaue digitale Milligramm-Waage zu investieren, um die Einnahme von sicheren und bekannten Dosen zu gewährleisten.<br />
+<br /><h2>Volumetrische Flüssigkeitsdosierung</h2>
+Bei der volumetrischen Flüssigdosierung wird eine Verbindung in einer Flüssigkeit aufgelöst, um sie leichter messen zu können.<br />
+Für Zwecke der Schadensbegrenzung ist es unerlässlich, bestimmte Substanzen, die zu stark sind, um sie mit herkömmlichen Waagen zu messen, in einer flüssigen Lösung zuzubereiten. Die meisten handelsüblichen Milligramm-Waagen können Dosen unter 10-15 mg nicht genau abwiegen. Mit dieser Technik ist es möglich, eine billige 30-Euro-Waage zu verwenden und trotzdem bis auf wenige Milligramm genau zu messen.<br />
+<br /><h2>Erste Hilfe</h2>
+Manchmal kann es vorkommen, dass trotz allen Sicherheitsvorkehrungen etwas passiert. Rufe im Zweifelsfall immer lieber einen Rettungsdienst! Mehr Informationen findest du unter anderem hier: <a href="https://tripscout.at/overdose">Überdosis</a>
+
+</p>
+  </div>
+</div>
+
+
+                
+                </div>
+            </div>
+        </section>
+        
+        <!-- Footer-->
+        @yield('frontend')
+                @include('frontend.footer')
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="{{ asset('js/scripts.js')}}"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+        <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/js/pages/dashboard.init.js') }}"></script>
+        <script src="{{ asset('backend/assets/js/app.js') }}"></script>
+        <script src="{{ asset('backend/assets/js/pages/datatables.init.js') }}"></script>
+
+
+</body>
+</html>
